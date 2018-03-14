5a66087b082dc73b0124ffecfd4efdfd2f315268
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2018-03-14 20:48:43 +0100

    Initial dump
    Need to clean up files!

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Fetch the latest release of pydPiper
+wget https://github.com/dhrone/pydPiper/archive/v0.3-alpha.tar.gz
+#wget $(curl -s https://api.github.com/repos/dhrone/pydPiper/releases/latest | grep 'tar' | cut -d\" -f4) -P /home/volumio/pydPiper
+
+mkdir /home/volumio/pydPiper
+tar zxvf v0.3-alpha.tar.gz -C /home/volumio/pydPiper/ --strip-components=1
+rm *.tar.gz
+
+cd /home/volumio/pydPiper
+sh ./install.sh
+
+wget -O /etc/systemd/system/pydpiper.service https://raw.githubusercontent.com/Saiyato/volumio-pydpiper-plugin/master/unit/pydpiper_minimized.service
+systemctl daemon-reload
+
+# timedatectl list-timezones
